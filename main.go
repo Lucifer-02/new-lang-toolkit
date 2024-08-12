897e511602c4f026d35a75dbd047d0ae6294bda9
HOANGNLV.HO <HOANGNLV.HO@VIETCOMBANK.COM.VN>
2024-08-12 10:39:31 +0700

    add modes

--- main.go
+++ main.go
@@ -6,15 +6,34 @@ import (
 )
 
 func main() {
-	source := os.Args[1]
-	target := os.Args[2]
-	text := os.Args[3]
 
-	translation := engines.GoogleTranslate(text, source, target)
+	if len(os.Args) != 5 {
+		//help
+		os.Stdout.Write([]byte("Usage: tool [mode] [source] [target] [text]\n"))
+		os.Stdout.Write([]byte("Modes:\n"))
+		os.Stdout.Write([]byte("tts: Text to speech\n"))
+		os.Stdout.Write([]byte("trans: Translate text\n"))
+		os.Stdout.Write([]byte("trans+tts: Translate text and convert to speech"))
+    return
+	}
 
-	engines.TTSConcurrent(translation, "vi")
-	audio := engines.TTSConcurrent(translation, "vi")
-	// engines.TTSConcurrent(translation, "vi")
+	mode := os.Args[1]
+	source := os.Args[2]
+	target := os.Args[3]
+	text := os.Args[4]
 
-	os.Stdout.Write(audio)
+	switch mode {
+	case "tts":
+		audio := engines.TTSConcurrent(text, target)
+		os.Stdout.Write(audio)
+	case "trans":
+		translation := engines.GoogleTranslate(text, source, target)
+		os.Stdout.Write([]byte(translation))
+	case "trans+tts":
+		translation := engines.GoogleTranslate(text, source, target)
+		audio := engines.TTSConcurrent(translation, target)
+		os.Stdout.Write(audio)
+	default:
+		panic("Invalid mode")
+	}
 }
